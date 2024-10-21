82dce024ce480633a813e5de07a59f0fe237d5a3
fujiwarat <takao.fujiwara1@gmail.com>
2024-10-21 20:48:23 +0900

    src/tests: Use dbus_try_get_local_machine_id() in ibus-share
    Replace dbus_get_local_machine_id() with dbus_try_get_local_machine_id()
    to retrieve the DBUsError.
    
    BUG=https://github.com/ibus/ibus/issues/2686

--- src/tests/ibus-share.c
+++ src/tests/ibus-share.c
@@ -4,10 +4,19 @@
 static void
 test_machine_id (void)
 {
+    DBusError error = DBUS_ERROR_INIT;
     const gchar *s1 = ibus_get_local_machine_id ();
-    gchar *s2 = dbus_get_local_machine_id ();
+    gchar *s2 = dbus_try_get_local_machine_id (&error);
 
-    g_assert_cmpstr (s1, ==, s2);
+    g_assert (s1);
+    if (!s2) {
+        g_warning ("Failed to get locale machine id: %s: %s",
+                   error.name, error.message);
+        dbus_error_free (&error);
+        g_assert_not_reached ();
+    } else {
+        g_assert_cmpstr (s1, ==, s2);
+    }
     dbus_free (s2);
 }
 
